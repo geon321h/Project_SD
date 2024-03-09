86170a806e0993271a57d328040f5b959f27bd88
geon <geon@DESKTOP-DT2DN7V>
2024-03-09 23:54:01 +0900

    색상추가

--- Project_SD/src/Style.java
+++ Project_SD/src/Style.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
+
 public class Style {
 
     Color mainColor = new Color(156, 93, 71);
@@ -9,8 +12,12 @@ public class Style {
     Color backgroundGray = new Color(235, 235, 235);
     Color inputWhite = new Color(255, 255, 255);
     Color inputGray = new Color(128, 128, 128);
+    Color DimGray = new Color(61, 61, 61);
     Color inputBlack = new Color(55, 55, 55);
-    Color menuBlack = new Color(61, 49, 45);
+    Color menuBlack = new Color(20, 12, 9);
+    
+    Color mainColor_thin = new Color(206, 115, 56);
+    Color headerColor = new Color(66, 46, 45);
     
 
     Font neo_H = new Font("나눔스퀘어 네오 Heavy", Font.PLAIN, 16);
@@ -18,6 +25,16 @@ public class Style {
     Font neo_B = new Font("나눔스퀘어 네오 Bold", Font.PLAIN, 14);
     Font neo_R = new Font("나눔스퀘어 네오 Regular", Font.PLAIN, 14);
     Font neo_L = new Font("나눔스퀘어 네오 light", Font.PLAIN, 14);
+    
+	public void composeJOptionPane() {
+
+		UIManager.put("OptionPane.background", Color.white);
+		UIManager.put("Panel.background", Color.white);
+		UIManager.put("Button.background", Color.white);
+		UIManager.put("Button.foreground", inputBlack);
+		UIManager.put("Button.font", neo_B.deriveFont((float)12));
+		
+	}
 
     // Font noto_M = new Font("Noto Sans KR Medium", Font.PLAIN, 14);
     // Font noto_B = new Font("Noto Sans KR Bold", Font.PLAIN, 14);
