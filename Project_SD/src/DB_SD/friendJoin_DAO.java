9cc9e3ee27a258523d2d036e5c39390020564b01
user <user@DESKTOP-PSCI4IT>
2024-03-11 13:23:54 +0900

    친구가 아니고 서로 친구 신청이 없는 유저만 찾는 메서드 추가

--- Project_SD/src/DB_SD/friendJoin_DAO.java
+++ Project_SD/src/DB_SD/friendJoin_DAO.java
@@ -147,5 +147,58 @@ public class friendJoin_DAO {
 		}
 		return lists;
 	}
+
+	public ArrayList<friendJoin_DTO> getBeforeFriendByName(int no, String searchName) {
+		connect();
+		String sql = "select userN.NO,userN.NAME "
+						+ "from (SELECT (CASE WHEN ? = USER_NO THEN FRieND_NO ELSE USER_NO END) AS FRIEND_NO "
+						+ "      FROM friendSD  "
+						+ "      where (USER_NO = ? OR FRieND_NO = ?) ) friendN "
+						+ "      right join userSD userN "
+						+ "      on friendN.FRIEND_NO = userN.NO "
+						+ "where (friendN.FRIEND_NO is null and not userN.NO = ?) and userN.NAME like ?";
+		
+		lists = new ArrayList<>();
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, no);
+			ps.setInt(2, no);
+			ps.setInt(3, no);
+			ps.setInt(4, no);
+			ps.setString(5, searchName);
+			rs = ps.executeQuery();
+			while(rs.next()) { 
+				int friend_no = rs.getInt("NO");
+				String name = rs.getString("NAME");
+
+				Dto = new friendJoin_DTO();
+				Dto.setNo(friend_no);
+				Dto.setName(name);
+				
+				lists.add(Dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				// 5.사용한 자원 반납
+				if(ps != null) {
+					ps.close();
+				}
+				if(rs != null){
+					rs.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}
+				System.out.println("접속 종료");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}			
+		}
+		return lists;
+		
+	}
 	
 }
